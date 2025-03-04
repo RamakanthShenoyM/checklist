68c7f52d873dea604d441620b562069c9ea20e9b
Jibin Jacob <jjaco@eg.dk>
2025-03-04 15:17:43 +0530

    Multiple options test checked

--- Engine.Tests/Unit/MultipleChoiceItemTest.cs
+++ Engine.Tests/Unit/MultipleChoiceItemTest.cs
@@ -10,38 +10,52 @@ using Xunit;
 
 namespace Engine.Tests.Unit
 {
-    public class MultipleChoiceItemTest
-    {
-        [Fact]
-        public void SingleItem()
-        {
-            var item = new MultipleChoiceItem(RedCarpet, GreenCarpet, NoCarpet);
-            var checklist = new Checklist(item);
-            Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
-            item.Be(GreenCarpet);
-            Assert.Equal(ChecklistStatus.Succeeded, checklist.Status());
-            item.Be(BlueCarpet);
-            Assert.Equal(ChecklistStatus.Failed, checklist.Status());
-            item.Reset();
-            Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
-        }
-        [Fact]
-        public void EmptyChecklist()
-        {
-            var item = new MultipleChoiceItem(RedCarpet);
-            var checklist = new Checklist(item);
-            Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
-            checklist.Cancel(item);
-            Assert.Equal(ChecklistStatus.NotApplicable, checklist.Status());
-        }
+	public class MultipleChoiceItemTest
+	{
+		[Fact]
+		public void SingleItem()
+		{
+			var item = new MultipleChoiceItem(RedCarpet, GreenCarpet, NoCarpet);
+			var checklist = new Checklist(item);
+			Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
+			item.Be(GreenCarpet);
+			Assert.Equal(ChecklistStatus.Succeeded, checklist.Status());
+			item.Be(BlueCarpet);
+			Assert.Equal(ChecklistStatus.Failed, checklist.Status());
+			item.Reset();
+			Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
+		}
+		[Fact]
+		public void EmptyChecklist()
+		{
+			var item = new MultipleChoiceItem(RedCarpet);
+			var checklist = new Checklist(item);
+			Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
+			checklist.Cancel(item);
+			Assert.Equal(ChecklistStatus.NotApplicable, checklist.Status());
+		}
+		[Fact]
+		public void MixedItems()
+		{
+			var item1 = new MultipleChoiceItem(RedCarpet, GreenCarpet, NoCarpet);
+			var item2 = new BooleanItem();
+			var item3 = new MultipleChoiceItem("India", "Iceland", "Norway");
+			var checklist = new Checklist(item1, item2, item3);
+			Assert.Equal(ChecklistStatus.InProgress, checklist.Status());
+			item1.Be(GreenCarpet);
+			item2.Be(true);
+			item3.Be("India");
+			Assert.Equal(ChecklistStatus.Succeeded, checklist.Status());
+			item3.Be("Poland");
+			Assert.Equal(ChecklistStatus.Failed, checklist.Status());
+		}
 
-
-    }
-    internal enum CarpetColor
-    {
-        RedCarpet, 
-        GreenCarpet, 
-        NoCarpet,
-        BlueCarpet
-    }
+	}
+	internal enum CarpetColor
+	{
+		RedCarpet,
+		GreenCarpet,
+		NoCarpet,
+		BlueCarpet
+	}
 }
