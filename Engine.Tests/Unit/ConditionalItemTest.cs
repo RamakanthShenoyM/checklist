effaf47ceef0c2da1a6576e26fe2ae8c50a0a73a
Fred George <fredgeorge@acm.org>
2025-03-28 13:10:20 +0530

    Fred: Using Checklist DSL in more tests

--- Engine.Tests/Unit/ConditionalItemTest.cs
+++ Engine.Tests/Unit/ConditionalItemTest.cs
@@ -6,46 +6,44 @@ using Xunit.Abstractions;
 using static Engine.Items.ChecklistStatus;
 using static Engine.Items.ChecklistExtensions;
 
-namespace Engine.Tests.Unit
-{
-	public class ConditionalItemTest(ITestOutputHelper testOutput)
-	{
-		private static readonly Person Creator = new(0, 0);
-		
-		[Fact]
-		public void Boolean()
-		{
-			var checklist = Creator.Checklist(
-				Conditional(
-				"Is US citizen?".TrueFalse(),
-				"Is Nevada resident?".TrueFalse(),
-				"Is Canadian citizen?".TrueFalse()
-			));
-			var baseItem = checklist.I(0, 0);
-			var successItem = checklist.I(0, 1);
-			var failItem = checklist.I(0, 2);
+namespace Engine.Tests.Unit {
+    public class ConditionalItemTest(ITestOutputHelper testOutput) {
+        private static readonly Person Creator = new(0, 0);
 
-			Assert.Equal(InProgress, checklist.Status());
-			Creator.Sets(baseItem).To(true);
-			Assert.Equal(InProgress, checklist.Status());
-			Creator.Sets(successItem).To(false);
-			Assert.Equal(Failed, checklist.Status());
-			Creator.Sets(baseItem).To(false);
-			Assert.Equal(InProgress, checklist.Status());
-			Creator.Sets(failItem).To(true);
-			Assert.Equal(Succeeded, checklist.Status());
-			var output = new PrettyPrint(checklist).Result();
-			testOutput.WriteLine(output);
-		}
-		
         [Fact]
-        public void BooleanWithFalse()
-        {
-            var baseItem = new BooleanItem("Is US citizen?");
-            var failItem = new BooleanItem("Is US citizen?");
+        public void Boolean() {
+            var checklist = Creator.Checklist(
+                Conditional(
+                    condition: "Is US citizen?".TrueFalse(),
+                    onSuccess: "Is Nevada resident?".TrueFalse(),
+                    onFailure: "Is Canadian citizen?".TrueFalse()
+                ));
+            var baseItem = checklist.I(0, 0);
+            var successItem = checklist.I(0, 1);
+            var failItem = checklist.I(0, 2);
 
-            var compositeItem = new ConditionalItem(baseItem, onFail: failItem);
-            var checklist = new Checklist( Creator, compositeItem);
+            Assert.Equal(InProgress, checklist.Status());
+            Creator.Sets(baseItem).To(true);
+            Assert.Equal(InProgress, checklist.Status());
+            Creator.Sets(successItem).To(false);
+            Assert.Equal(Failed, checklist.Status());
+            Creator.Sets(baseItem).To(false);
+            Assert.Equal(InProgress, checklist.Status());
+            Creator.Sets(failItem).To(true);
+            Assert.Equal(Succeeded, checklist.Status());
+            var output = new PrettyPrint(checklist).Result();
+            testOutput.WriteLine(output);
+        }
+
+        [Fact]
+        public void BooleanWithFalse() {
+            var checklist = Creator.Checklist(
+                Conditional(
+                    condition: "Is US citizen?".TrueFalse(),
+                    onFailure: "Is Canadian citizen?".TrueFalse()
+                ));
+            var baseItem = checklist.I(0, 0);
+            var failItem = checklist.I(0, 2);
 
             Assert.Equal(InProgress, checklist.Status());
             Creator.Sets(baseItem).To(true);
@@ -55,14 +53,16 @@ namespace Engine.Tests.Unit
             Creator.Sets(failItem).To(true);
             Assert.Equal(Succeeded, checklist.Status());
         }
-        [Fact]
-        public void BooleanWithTrue()
-        {
-            var baseItem = new BooleanItem("Is US citizen?");
-            var successItem = new BooleanItem("Is US citizen?");
 
-            var compositeItem = new ConditionalItem(baseItem, onSuccess: successItem);
-            var checklist = new Checklist( Creator, compositeItem);
+        [Fact]
+        public void BooleanWithTrue() {
+            var checklist = Creator.Checklist(
+                Conditional(
+                    condition: "Is US citizen?".TrueFalse(),
+                    onSuccess: "Is Nevada resident?".TrueFalse()
+                ));
+            var baseItem = checklist.I(0, 0);
+            var successItem = checklist.I(0, 1);
 
             Assert.Equal(InProgress, checklist.Status());
             Creator.Sets(baseItem).To(false);
@@ -71,30 +71,29 @@ namespace Engine.Tests.Unit
             Assert.Equal(InProgress, checklist.Status());
             Creator.Sets(successItem).To(true);
             Assert.Equal(Succeeded, checklist.Status());
-            Assert.Throws<InvalidOperationException>(() => Creator.Sets(compositeItem).To(true));
         }
-        
+
         [Fact]
         public void ConditionalWithConditional() {
-	        var checklist = Creator.Checklist(
-		        "First simple item".TrueFalse(),
-		        Conditional(
-			        condition: "First condition".TrueFalse(),
-			        onSuccess: Conditional(
-				        condition: "Second condition".TrueFalse(),
-				        onSuccess: "Second success leg".TrueFalse(),
-				        onFailure: "Second failure leg".TrueFalse()
-			        ),
-			        onFailure: Not(
-				        Or(
-					        "First Or of first failure leg".TrueFalse(),
-					        "Second Or of first failure leg".TrueFalse()
-				        )
-			        )
-		        ),
-		        "Last simple item".TrueFalse()
-	        );
-	        testOutput.WriteLine(checklist.ToString());
+            var checklist = Creator.Checklist(
+                "First simple item".TrueFalse(),
+                Conditional(
+                    condition: "First condition".TrueFalse(),
+                    onSuccess: Conditional(
+                        condition: "Second condition".TrueFalse(),
+                        onSuccess: "Second success leg".TrueFalse(),
+                        onFailure: "Second failure leg".TrueFalse()
+                    ),
+                    onFailure: Not(
+                        Or(
+                            "First Or of first failure leg".TrueFalse(),
+                            "Second Or of first failure leg".TrueFalse()
+                        )
+                    )
+                ),
+                "Last simple item".TrueFalse()
+            );
+            testOutput.WriteLine(checklist.ToString());
         }
     }
 }
