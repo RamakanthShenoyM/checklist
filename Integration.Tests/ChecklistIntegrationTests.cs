5d94b0517a9cee09dedd98ff88855622a21b083c
Prajnesh Noochila <prnoo@eg.dk>
2025-04-11 13:48:10 +0530

    Created integration project

---
+++ Integration.Tests/ChecklistIntegrationTests.cs
@@ -0,0 +1,14 @@
+
+using CommandEngine.Commands;
+using CommandEngine.Tasks;
+
+namespace Integration.Tests
+{
+    public class ChecklistIntegrationTests
+    {
+        [Fact]
+        public void ChecklistInteractionWithCommand()
+        {
+        }
+    }
+}
